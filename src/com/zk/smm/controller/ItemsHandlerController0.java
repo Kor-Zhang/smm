5510bc360cb0a736a8dd9552c7d68231f9692598
Kor Zhang <1138829222@qq.com>
2017-09-05 11:15:51 +0800

    配置并且成功测试了sppringmvc框架.
    配置并且成功测试了sppringmvc框架.

--- src/com/zk/smm/controller/ItemsHandlerController0.java
+++ src/com/zk/smm/controller/ItemsHandlerController0.java
@@ -1,10 +1,15 @@
 package com.zk.smm.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
+
+import com.zk.smm.bean.Items;
 
 /**
  * Title:ItemsHandlerController0
@@ -20,8 +25,32 @@ public class ItemsHandlerController0 implements Controller{
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
+		/**
+		 * 模拟处理数据
+		 */
+		List<Items> items = new ArrayList<Items>();
+		int index = 0;
+		Items it;
+		while(index++ < 10){
+			it = new Items();
+			it.setDescription(""+index);
+			it.setId(""+index);
+			it.setName(""+index);
+			it.setOrderDetails(null);
+			it.setPrice(Double.valueOf(index));
+			it.setQuantity(index);
+			items.add(it);
+		}
 		
-		return null;
+		
+		ModelAndView mav = new ModelAndView();
+		//指定数据
+		mav.addObject("items",items);
+		//指定视图
+		mav.setViewName("/WEB-INF/view/items/itemsList.jsp");
+		
+		
+		return mav;
 	}
 
 }
